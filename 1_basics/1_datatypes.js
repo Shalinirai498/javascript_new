9e3012108886c94d550504e9a9975e5b3d37b27e
Shalinirai498 <115859504+Shalinirai498@users.noreply.github.com>
2024-05-01 08:03:25 +0000

    datatypes

---
+++ 1_basics/1_datatypes.js
@@ -0,0 +1,11 @@
+"use strict";  //treat all js code as newer version
+//alert(3+3); we are using node not browser
+//number,bigint,string=> "",''
+//boolean=>true,false
+//null=>standalone value
+//undefined
+//symbol=>unique
+//object
+console.log(typeof "shalini")
+console.log(typeof null)//object
+console.log(typeof undefined)//undefined
