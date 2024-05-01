d2124ec0ea61aa682950b0cfe156e006cc20eaf5
Shalinirai498 <115859504+Shalinirai498@users.noreply.github.com>
2024-05-01 09:51:06 +0000

    conversion of datatype

---
+++ 1_basics/1_conversionOperation.js
@@ -0,0 +1,22 @@
+let score="33" 
+console.log(typeof score)
+let value=Number(score)
+console.log(typeof value)
+//"33"=>33
+//"33"=>NaN
+//"33abc"=NaN
+//null=>0
+//undefined=>NaN
+//true=>1
+let isloggedin=1;
+let booleanisloggedin=Boolean(isloggedin) 
+console.log(booleanisloggedin)
+//1=> true;0=> false
+//""=>false
+//"shalini"=>true
+let someNumber=33
+let stringNumber=String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
+
+
