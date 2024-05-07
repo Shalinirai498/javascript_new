7fe02d3af3d5ae40d5f973bac293487384316108
Shalinirai498 <115859504+Shalinirai498@users.noreply.github.com>
2024-05-07 08:21:00 +0000

    modified string

--- 1_basics/1_strings.js
+++ 1_basics/1_strings.js
@@ -1,4 +1,28 @@
-const name="shalini"
-const age=19
-console.log(name+age+"Value");
-console.log(`hello my name is `);
+// const name="shalini"
+// const age=19
+// console.log(name+age+"Value");
+// console.log(`hello my name is ${name} and my age is ${age}`);
+const getname=new String("shalini_rai")
+// console.log(getname[0]);
+// console.log(getname.__proto__);
+// console.log(getname.length);
+// console.log(getname.toUpperCase());
+// console.log(getname.charAt(4));
+// console.log(getname.indexOf('r'));
+// const newstring=getname.substring(1,5)
+//console.log(newstring);
+const anotherstring=getname.slice(8,11)
+console.log(anotherstring);
+const newStringOne = "   hitesh    "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+const url = "https://hitesh.com/hitesh%20choudhary"
+
+console.log(url.replace('%20', '-'))
+
+console.log(url.includes('sundar'))
+
+console.log(getname.split('_'));
+
+
